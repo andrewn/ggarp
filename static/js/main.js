0a74e11cf72291edacfd25cc290ccd825d5af642
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-06-25 14:11:09 +0100

    Tidy up more-info into it's own function

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,18 @@
 
 require(['jquery'], function( $ ) {
-    
-   $(document).ready(function() {
+  
+  $(document).ready(function() {
+    initMoreInfo();
+  });
 
+   function initMoreInfo() {
         var moreInfoControl = $('.more-info'),
             moreInfoContent = $('.more');
 
         moreInfoControl.click(function(evt){
             moreInfoContent.toggle('fast');
             return false;
-        });
-   });
+        });       
+   }
 
 });
