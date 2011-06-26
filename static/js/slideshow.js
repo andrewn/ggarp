df31a840968846e618f98f981800ee9f2099e132
Andrew Nicolaou <public@andrewnicolaou.co.uk>
2011-06-26 09:26:46 +0100

    refactor thumbs to manager class

--- static/js/slideshow.js
+++ static/js/slideshow.js
@@ -1,5 +1,8 @@
 define(['jquery', 'util'], function ($, util) {
-     
+    
+    var KEY_RIGHT = 39,
+        KEY_LEFT  = 37;
+
     var Slideshow = function( container, opts ) {
 
         var opts = opts || {},
@@ -8,7 +11,7 @@ define(['jquery', 'util'], function ($, util) {
             },
             container       = $( container ),
             thumbsContainer = container.find('.thumbnails'),
-            thumbs          = thumbsContainer.children(),
+            useKeyboardNav  = (opts.useKeyboardNav != null) ? opts.useKeyboardNav : true,
             mainSlideContainer,
             slideManager,
             slides;
@@ -49,34 +52,97 @@ define(['jquery', 'util'], function ($, util) {
             return id;
         }
 
-        // Set-up slide effects and click events on each item
-        thumbs.each( function ( index, item ){
-            var item = $( item );
-            item.click(
-                function (evt) {
-                    thumbs.removeClass('selected');
-                    
-                    var slideId = slideIdFromUrl( item.children('a').attr('href') );
-                    slideManager.show( slideId );
-                    item.addClass( 'selected' );
-                    return false;
-                }
-            );
+        thumbManager = new ThumbManager(thumbsContainer);
+        // jQuery silliness: bind to dom element to receive events
+        thumbsContainer.bind('selected', function (evt, thumb) {            
+            var slideId = slideIdFromUrl( $(thumb).children('a').attr('href') );
+            slideManager.show( slideId );        
         });
+
+        // Keyboard nav
+        if (useKeyboardNav) {
+            $(document).keyup(function(evt) {
+               switch(evt.which) {
+                   case KEY_RIGHT:  showSlide('next');
+                                    break;
+                   case KEY_LEFT:   showSlide('prev');
+                                    break;
+               }
+            })
+        }
+
+        function showSlide(direction) {
+            // var currentSlide = thumbs.filter('.selected');
+            // console.log('currentSlide', currentSlide, thumbs);
+            var currentSlide = slideManager.currentSlideId,
+                newSlide;
+            if ( direction == 'next' && slideManager.hasSlide(currentSlide + 1) ) {
+                newSlide = currentSlide + 1;
+                slideManager.show(newSlide);
+                //thumbs.get(currentSlide).removeClass('selected');
+                //thumbs.get(newSlide).addClass('selected');
+            }
+        }
         
         checkIfImageShouldBeDisplayed( document.location.hash );
         
         function checkIfImageShouldBeDisplayed( hash ) {
-            if ( hash ) {
-                var match = hash.match(/#(.*)$/);
-                var queryStringId = ( match.length > 1 ) ? match[1] : "";
-                slideManager.show( queryStringId );
-            } else {
+           // if ( hash ) {
+           //     var match = hash.match(/#(.*)$/);
+           //     var queryStringId = ( match.length > 1 ) ? match[1] : "";
+           //     slideManager.show( queryStringId );
+           //     thumbManager.show( queryStringId.replace('media-', '') );
+           // } else {
                 slideManager.showFirst();
-                thumbs.first().addClass('selected');
-            }
+                thumbManager.showFirst();
+           // }
         }
         
+
+        function ThumbManager(sel) {
+            var container = $(sel),
+                thumbs    = container.children(),
+                current;
+
+            // Set-up slide effects and click events on each item
+            thumbs.each( function ( index, item ){
+                var item = $( item );
+                item.click(
+                    function (evt) {
+                        thumbs.removeClass('selected');
+                        item.addClass('selected');
+
+                        container.trigger('selected', item);
+                        
+                        return false;
+                    }
+                );
+            });
+            return {
+                showFirst: function () {
+                    this.show(0)
+                },
+                showNext: function () {
+                    var next = current + 1;
+                    if ( next < thumbs.length ) {
+                        this.show(next)
+                    }
+                },
+                showPrevious: function () {
+                    var prev = current - 1;
+                    if ( prev >= 0 ) {
+                        this.show(prev)
+                    }
+                },
+                show: function (index) {
+                    thumbs.removeClass('selected');
+                    if (thumbs.get(index)) {
+                        $(thumbs.get(index)).addClass('selected');
+                    }
+                    current = index;
+                }
+            }
+        }
         
         /**
           * SlideManager
@@ -116,6 +182,7 @@ define(['jquery', 'util'], function ($, util) {
                         }
                     );
                 }
+                this.currentSlideId = id;
             }
             
             function fadeOut( el, cb ) {
@@ -132,6 +199,9 @@ define(['jquery', 'util'], function ($, util) {
                 show    : show,
                 showFirst: function() {
                     show( firstImageId );
+                },
+                hasSlide : function (id) {
+    //console.log(id)  
                 },
                 cache   : imageCache
             }
